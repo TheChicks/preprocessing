a15c11ed93ecf1f4998be9248144ddc989b53092
DongHee Kim <opklnm102@gmail.com>
2016-04-08 19:41:55 +0900

    Project home 수정

---
+++ imageProcessing/src/imageProcessing/PicturePanel.java
@@ -0,0 +1,34 @@
+package imageProcessing;
+
+import java.awt.*;  
+import java.awt.image.BufferedImage;  
+import javax.swing.*;  
+import org.opencv.core.Mat;  
+import org.opencv.imgcodecs.*;
+public class PicturePanel extends JPanel{   //화면 출력 클래스
+	  Mat mat;  
+	  BufferedImage buf_image; 
+	
+	  public PicturePanel(String imgName, String fileExt) throws Exception{
+	      super(); // Calls the parent constructor
+	    
+	    //The input image file is not "right" if it has no columns!
+	      if( Imgcodecs.imread(imgName).cols() != 0){  
+	    	  mat = Imgcodecs.imread(imgName);
+	    
+	    	  EditMat em =  new EditMat(mat, buf_image); //픽셀변경 클래스 호출
+	    	  mat = em.edit();
+	    	  
+	    	  MatToBufImg converter = new MatToBufImg(mat, fileExt);
+	    	  buf_image = converter.getImage();
+	    	    	  
+	      }
+	      else throw new Exception();
+	  	}
+	  
+	   //Override default paint
+	   public void paintComponent(Graphics g){  
+	     g.drawImage(buf_image,50,50,mat.width(),mat.height(), this);
+	  }  
+
+} 
